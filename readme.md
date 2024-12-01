173acd049c33ca7721414e6bc28e733175d1430b
prasolay2020 <prasolay2020@gmail.com>
2024-12-01 15:15:36 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+1. npm install ol@9.2.4
+2. npm install --save-dev @babel/core typescript @babel/preset-env
+3. npm install express
+4. npm install --save-dev @types/node 
+5. tsc --init
+6. npm install --save-dev parcel
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import Map from "ol/Map.js";
+import OSM from "ol/source/OSM.js";
+import TileLayer from "ol/layer/Tile.js";
+import View from "ol/View.js";
+
+const map = new Map({
+  target: "map",
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
+
+function helloFunction(user: string) {
+  console.log(`Hello ${user}`);
+}
+
+helloFunction("使用者1號");
